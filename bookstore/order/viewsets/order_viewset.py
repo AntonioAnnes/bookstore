54e4e43631478a6b36015945c0d3b47168b81d86
Antonio Annes <antonio.vmachado13@gmail.com>
2023-08-01 21:50:50 -0300

    adding token order

--- bookstore/order/viewsets/order_viewset.py
+++ bookstore/order/viewsets/order_viewset.py
@@ -1,11 +1,16 @@
-
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from bookstore.order.models import Order
 from bookstore.order.serializers import OrderSerializer
+from bookstore.product.serializers import ProductSerializer
 
 
 class OrderViewSet(ModelViewSet):
+        authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+        permission_classes = [IsAuthenticated]
+        serializer_class = ProductSerializer
 
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
